4fe844a294e2404e9836417de63d49a05b77a99f
Matthew Semroska <matthew.semroska@gmail.com>
2021-05-18 00:47:25 -0600

    helloWorld, data Binding, conditional rendering, template iteration

---
+++ README.md
@@ -0,0 +1,5 @@
+Lightning Web Components Lessons
+
+1. force-app\main\default\lwc\helloWorld - This item pertains to the video titled "Hello World LWC" 
+2. force-app\main\default\lwc\dataBinding - Data Binding
+		
---
+++ force-app/main/default/lwc/dataBinding/dataBinding.js
@@ -0,0 +1,34 @@
+import { LightningElement } from 'lwc';
+
+export default class DataBinding extends LightningElement {
+    str = 'hello I am a string';
+    arr = ['a', 'b', 'c'];
+    obj = {
+        name: 'Matt',
+        height: `6'2"`
+    }
+    bool = false;
+    conditional = "";
+
+    changeHandler(event) {
+        this.conditional = event.target.value;
+    }
+
+    players = [
+        {
+            name : 'John Macintosh',
+            age: '21',
+            height:`6'4"`
+        },
+        {
+            name : 'Cheryl Quandry',
+            age: '24',
+            height:`5'7"`
+        },
+        {
+            name : 'Michael Jameson',
+            age: '22',
+            height:`6'1"`
+        },
+    ]
+}
